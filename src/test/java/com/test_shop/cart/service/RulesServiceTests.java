6a6ef7eaf60a36ce9dfcdbeec3cb8993bf648a0e
Lando <lando.vargas@dep.cl>
2026-02-12 00:47:02 -0300

    feat: one more tests for ruleservice

--- src/test/java/com/test_shop/cart/service/RulesServiceTests.java
+++ src/test/java/com/test_shop/cart/service/RulesServiceTests.java
@@ -7,7 +7,7 @@ import com.test_shop.cart.service.rules.PromoRule;
 import com.test_shop.cart.service.rules.targets.ProductList;
 import com.test_shop.cart.service.rules.targets.RuleTarget;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -30,5 +30,23 @@ public class RulesServiceTests {
         ProductService b = new ProductService("Leche", colun, new MoneyService(new BigDecimal("1500")));
  
         assertTrue(rs.isInTargetList(b));
+    }
+
+    @Test
+    void testRuleServiceNotFindsProduct(){
+
+        String description = "A test ruleservice";
+        byte weight = 10;
+        BrandService colun = new BrandService("Colun");
+        ProductService a = new ProductService("Leche", colun, new MoneyService(new BigDecimal("1500")));
+        RuleTarget target = new ProductList(new ArrayList<>(List.of(a)));
+        boolean stackWithOtherRules = false;
+        KnownRules rule = new PromoRule();
+
+        RulesService rs = new RulesService(description, weight, target, stackWithOtherRules, rule);
+
+        ProductService b = new ProductService("Arroz", new BrandService("Miraflores"), new MoneyService(new BigDecimal("1500")));
+ 
+        assertFalse(rs.isInTargetList(b));
     }
 }
